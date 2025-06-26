c48f44fd486eee936cc2d105fc0e0330aae9eb40
DreamHigh0820 <dreamhigh0820@gmail.com>
2025-06-26 13:09:32 -0400

    init

---
+++ vite.config.js
@@ -0,0 +1,54 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import { nodePolyfills } from 'vite-plugin-node-polyfills'; 
+
+
+export default defineConfig({
+  plugins: [
+    react(),
+    
+    nodePolyfills({
+      
+      
+      
+      global: true,
+      
+      buffer: true,
+      
+      process: true,
+    }),
+  ],
+  define: {
+    
+    'global': 'globalThis',
+  },
+   
+  server: {
+    cors: {
+      origin: '*', 
+      methods: ['GET', 'POST', 'PUT', 'DELETE'],
+      allowedHeaders: ['Content-Type', 'Authorization'],
+      credentials: true,
+    },
+    host: true, 
+    port: 5173, 
+  },
+  resolve: {
+    alias: {
+      
+      
+    }
+  },
+  build: {
+    target: 'esnext', 
+     sourcemap: true, 
+  },
+   optimizeDeps: {
+     esbuildOptions: {
+       
+       define: {
+         global: 'globalThis',
+       },
+     },
+   },
+});
